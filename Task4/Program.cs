9ce13898874cf1e16d117be29f5552ab31feb448
Poloshak <e.l.fomenko@yandex.ru>
2022-12-02 00:58:04 +0300

    Домашка к семинару 01 12 2022

---
+++ Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+Console.WriteLine("a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("b: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("c: ");
+int c = int.Parse(Console.ReadLine());
+if (a>b){
+    if (a>c){
+         Console.Write($"max={a}");
+    }else{
+         Console.Write($"max={c}");
+    }
+}else{
+    if (b>c){
+         Console.Write($"max={b}");
+    }else{
+         Console.Write($"max={c}");
+    }
+}
